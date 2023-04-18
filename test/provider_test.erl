429fe9cdc9fae7de1a62dc0e07d40c1feb4d46fc
joq62 <jakim.leche@gmail.com>
2023-04-18 23:03:27 +0200

    corr

--- test/provider_test.erl
+++ test/provider_test.erl
@@ -43,35 +43,37 @@ read_specs_test(Node)->
     
     AllProviders=lists:sort(rpc:call(Node,db_provider_spec,get_all_id,[],5000)),
 %    io:format("AllProviders ~p~n",[{AllProviders,?MODULE,?FUNCTION_NAME}]),
-    true=lists:member("dbetcd",AllProviders),
+    true=lists:member("dbetcd_appl",AllProviders),
 
     {
-     "dbetcd","dbetcd","0.1.0","dbetcd",dbetcd,"dbetcd","dbetcd","a_cookie",
-     "https://github.com/joq62/dbetcd.git",
-     " -pa dbetcd/lib/*/ebin -config dbetcd/config/sys.config",
-     "tar -xvf dbetcd/dbetcd-0.1.0.tar.gz -C dbetcd ",
-     {application,start,[dbetcd],20000},
-     1,[all_hosts]
-    }=rpc:call(Node,db_provider_spec,read,["dbetcd"],5000),
+     "dbetcd_appl","dbetcd_appl","0.1.0","dbetcd_appl",dbetcd_appl,
+     "dbetcd_appl","dbetcd_appl","a_cookie",
+     "https://github.com/joq62/dbetcd_appl.git",
+     " -pa dbetcd_appl/_build/default/lib/*/ebin -config dbetcd_appl/config/sys.config",
+     "tar -xvf dbetcd_appl/dbetcd_appl-0.1.0.tar.gz -C dbetcd_appl ",
+     {application,start,[dbetcd_appl],20000},
+     1,
+     [all_hosts]
+    }=rpc:call(Node,db_provider_spec,read,["dbetcd_appl"],5000),
     
-    {ok,"dbetcd"}=rpc:call(Node,db_provider_spec,read,[spec,"dbetcd"],5000),
-    {ok,"dbetcd"}=rpc:call(Node,db_provider_spec,read,[appl_name,"dbetcd"],5000),
-    {ok,"0.1.0"}=rpc:call(Node,db_provider_spec,read,[vsn,"dbetcd"],5000),
-    {ok,"dbetcd"}=rpc:call(Node,db_provider_spec,read,[app_name,"dbetcd"],5000),
-    {ok,dbetcd}=rpc:call(Node,db_provider_spec,read,[app,"dbetcd"],5000),
-    {ok,"dbetcd"}=rpc:call(Node,db_provider_spec,read,[dir,"dbetcd"],5000),
-    {ok,"dbetcd"}=rpc:call(Node,db_provider_spec,read,[node_name,"dbetcd"],5000),
-    {ok,"a_cookie"}=rpc:call(Node,db_provider_spec,read,[cookie,"dbetcd"],5000),
-    {ok," -pa dbetcd/lib/*/ebin -config dbetcd/config/sys.config"}=rpc:call(Node,db_provider_spec,read,[pa_args,"dbetcd"],5000),
-    {ok,"https://github.com/joq62/dbetcd.git"}=rpc:call(Node,db_provider_spec,read,[git_path,"dbetcd"],5000),
-    {ok,"tar -xvf dbetcd/dbetcd-0.1.0.tar.gz -C dbetcd "}=rpc:call(Node,db_provider_spec,read,[tar_cmd,"dbetcd"],5000),
-    {ok,{application,start,[dbetcd],20000}}=rpc:call(Node,db_provider_spec,read,[start_cmd,"dbetcd"],5000),
-    {ok,1}=rpc:call(Node,db_provider_spec,read,[num,"dbetcd"],5000),
-    {ok, [all_hosts]}=rpc:call(Node,db_provider_spec,read,[affinity,"dbetcd"],5000),
+    {ok,"dbetcd_appl"}=rpc:call(Node,db_provider_spec,read,[spec,"dbetcd_appl"],5000),
+    {ok,"dbetcd_appl"}=rpc:call(Node,db_provider_spec,read,[appl_name,"dbetcd_appl"],5000),
+    {ok,"0.1.0"}=rpc:call(Node,db_provider_spec,read,[vsn,"dbetcd_appl"],5000),
+    {ok,"dbetcd_appl"}=rpc:call(Node,db_provider_spec,read,[app_name,"dbetcd_appl"],5000),
+    {ok,dbetcd_appl}=rpc:call(Node,db_provider_spec,read,[app,"dbetcd_appl"],5000),
+    {ok,"dbetcd_appl"}=rpc:call(Node,db_provider_spec,read,[dir,"dbetcd_appl"],5000),
+    {ok,"dbetcd_appl"}=rpc:call(Node,db_provider_spec,read,[node_name,"dbetcd_appl"],5000),
+    {ok,"a_cookie"}=rpc:call(Node,db_provider_spec,read,[cookie,"dbetcd_appl"],5000),
+    {ok," -pa dbetcd_appl/_build/default/lib/*/ebin -config dbetcd_appl/config/sys.config"}=rpc:call(Node,db_provider_spec,read,[pa_args,"dbetcd_appl"],5000),
+    {ok,"https://github.com/joq62/dbetcd_appl.git"}=rpc:call(Node,db_provider_spec,read,[git_path,"dbetcd_appl"],5000),
+    {ok,"tar -xvf dbetcd_appl/dbetcd_appl-0.1.0.tar.gz -C dbetcd_appl "}=rpc:call(Node,db_provider_spec,read,[tar_cmd,"dbetcd_appl"],5000),
+    {ok,{application,start,[dbetcd_appl],20000}}=rpc:call(Node,db_provider_spec,read,[start_cmd,"dbetcd_appl"],5000),
+    {ok,1}=rpc:call(Node,db_provider_spec,read,[num,"dbetcd_appl"],5000),
+    {ok, [all_hosts]}=rpc:call(Node,db_provider_spec,read,[affinity,"dbetcd_appl"],5000),
     
  
     {error,[eexist,"glurk",db_provider_spec,_]}=rpc:call(Node,db_provider_spec,read,[dir,"glurk"],5000),
-    {error,['Key eexists',glurk,"dbetcd",db_provider_spec,_]}=rpc:call(Node,db_provider_spec,read,[glurk,"dbetcd"],5000),
+    {error,['Key eexists',glurk,"dbetcd_appl",db_provider_spec,_]}=rpc:call(Node,db_provider_spec,read,[glurk,"dbetcd_appl"],5000),
  
        ok.
 
